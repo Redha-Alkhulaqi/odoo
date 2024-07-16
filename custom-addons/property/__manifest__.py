5574d2d82b9b8cca93eb1fb8cecabc6d2a4f02e7
Redha-Alkhulaqi <reda.ali.ahmed@gmail.com>
2024-07-16 13:57:56 +0300

    edit addons "custom-addons"

--- custom-addons/property/__manifest__.py
+++ custom-addons/property/__manifest__.py
@@ -9,6 +9,7 @@
     """,
     # data files always loaded at installation
     'data': [
+        'security/ir.model.access.csv',
         'views/base_menu.xml',
 #       'views/property_view.xml',
     ],
