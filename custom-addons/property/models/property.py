5668a4eaa1ced3b5321735801e0919c5d8b5a890
Redha-Alkhulaqi <reda.ali.ahmed@gmail.com>
2024-07-17 14:00:15 +0300

    update file of "property.py"

--- custom-addons/property/models/property.py
+++ custom-addons/property/models/property.py
@@ -29,6 +29,7 @@ class Property(models.Model):
     ], default='draft')
     owner_id = fields.Many2one('owner')
     #taq_ids = fields.Many2many('taq')
+
     _sql_constraints = [
     ('unique_name', 'unique("name")', 'This name is exist!')
     ]
