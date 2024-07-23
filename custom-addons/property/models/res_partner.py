9f9d56446b100aba97ccf950b88c61775d1a5487
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-07-23 11:16:43 +0300

    Adding a price Field​ to Contact.

--- custom-addons/property/models/res_partner.py
+++ custom-addons/property/models/res_partner.py
@@ -4,8 +4,8 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    property_id = fields.Many2one('property')
-    price = fields.Float(related='property_id.selling_price', readonly=False)
+    #property_id = fields.Many2one('property')
+    #price = fields.Float(related='property_id.selling_price', readonly=False)
     # price = fields.Float(compute='_compute_price', readonly=False)
 
     # @api.depends('property_id')
