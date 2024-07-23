7dd84e1e945a67fb43b51d365586401e69f37f90
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-07-23 16:23:00 +0300

    Reserved Field Names & Archiving/Unarchiving Technique.

--- custom-addons/property/models/building.py
+++ custom-addons/property/models/building.py
@@ -4,6 +4,7 @@ class Building(models.Model):
     _name = 'building'
     _description = 'Building Record'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'code'
 
     no = fields.Integer()
     code = fields.Char()
