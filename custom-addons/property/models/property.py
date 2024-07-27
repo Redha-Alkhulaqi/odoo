efa49823892ced2d33d4ee51bcf958ef0f00e56c
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-07-27 13:42:35 +0300

    Enforcing Edit Restrictions and​ Cross-Model Creation.

--- custom-addons/property/models/property.py
+++ custom-addons/property/models/property.py
@@ -63,18 +63,22 @@ class Property(models.Model):
 
     def action_draft(self):
         for rec in self:
+            rec.create_history_record(rec.state, 'draft')
             rec.state = 'draft'
 
     def action_pending(self):
         for rec in self:
+            rec.create_history_record(rec.state, 'pending')
             rec.state = 'pending'
 
     def action_sold(self):
         for rec in self:
+            rec.create_history_record(rec.state, 'sold')
             rec.state = 'sold'
 
     def action_closed(self):
         for rec in self:
+            rec.create_history_record(rec.state, 'closed')
             rec.state = 'closed'
 
     def check_expected_selling_date(self):
@@ -89,6 +93,16 @@ class Property(models.Model):
         if res.ref == 'New':
             res.ref = self.env['ir.sequence'].next_by_code('property_seq')
         return res
+
+    def create_history_record(self, old_state, new_state):
+        for rec in self:
+            rec.env['property.history'].create({
+                'user_id': rec.env.uid,
+                'property_id': rec.id,
+                'old_state': old_state,
+                'new_state': new_state,
+
+            })
 
 class PropertyLine(models.Model):
     _name = 'property.line'
