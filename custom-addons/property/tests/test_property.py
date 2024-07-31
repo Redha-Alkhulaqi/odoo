064cfb35161e6fa4f03c3fd2b99e47373ae55f2c
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-07-31 10:22:36 +0300

    Adding Unit Test To Your App.

--- custom-addons/property/tests/test_property.py
+++ custom-addons/property/tests/test_property.py
@@ -4,7 +4,7 @@ from odoo import fields
 class TestProperty(TransactionCase):
     
     def setup(self, *args, **kwargs):
-        super(TestProperty, self).setup()
+        super(TestProperty, self).setUp()
 
         self.property_01_record = self.env['property'].create({
             'ref': 'PRT10000',
@@ -30,7 +30,7 @@ class TestProperty(TransactionCase):
     def test_01_property_values(self):
         property_id = self.property_01_record
 
-        self.assertRecordValues(property_id,[{
+        self.assertRecordValues(property_id, [{
             'ref': 'PRT10000',
             'name': 'Property 10000',
             'description': 'Property 10000 description',
