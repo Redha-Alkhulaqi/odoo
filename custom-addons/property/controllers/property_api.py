651335612b11b2205e6c063d103bb68fb72a0e9a
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-08-03 15:43:49 +0300

    APIs in Odoo: Part 3 (CRUD) 2. Update Operation.

--- custom-addons/property/controllers/property_api.py
+++ custom-addons/property/controllers/property_api.py
@@ -38,12 +38,21 @@ class PropertyApi(http.Controller):
 
     @http.route("/v1/property/<int:property_id>", methods=["PUT"], type="http", auth="none", csrf=False)
     def update_property(self, property_id):
-        property_id = request.env['property'].sudo().search([('id', '=', property_id)])
-        args = request.httprequest.data.decode()
-        vals = json.loads(args)
-        property_id.write(vals)
-        return request.make_json_response({
-            "message": "Property has been updated successfully",
-            "id": property_id.id,
-            "name": property_id.name,
-        }, status=200)
+        try:
+            property_id = request.env['property'].sudo().search([('id', '=', property_id)])
+            if not property_id:
+                return request.make_json_response({
+                    "message": "ID dose not exist!",
+                }, status=400)
+            args = request.httprequest.data.decode()
+            vals = json.loads(args)
+            property_id.write(vals)
+            return request.make_json_response({
+                "message": "Property has been updated successfully",
+                "id": property_id.id,
+                "name": property_id.name,
+            }, status=200)
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
