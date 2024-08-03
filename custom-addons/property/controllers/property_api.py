62c1cac351e9271e8079b55135caca0dde5d628f
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-08-03 13:23:37 +0300

    APIs in Odoo: Part 2 (CRUD) 1. Create Operation (Part 2).

--- custom-addons/property/controllers/property_api.py
+++ custom-addons/property/controllers/property_api.py
@@ -9,13 +9,18 @@ class PropertyApi(http.Controller):
     def post_property(self):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
-        res = request.env['property'].sudo().create(vals)
-        if res:
+        try:
+            res = request.env['property'].sudo().create(vals)
+            if res:
+                return request.make_json_response({
+                    "message": "Property has been created successfully",
+                    "id": res.id,
+                    "name": res.name,
+                }, status=201)
+        except Exception as error:
             return request.make_json_response({
-                "message": "Property has been created successfully",
-                "id": res.id,
-                "name": res.name,
-            }, status=201)
+                "message": error,
+            }, status=400)
 
     @http.route("/v1/property/json", methods=["POST"], type="json", auth="none", csrf=False)
     def post_property_json(self):
