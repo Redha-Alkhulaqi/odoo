acd41b76bf04521ab6910f0cbe269443968c4001
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-08-04 10:00:17 +0300

    APIs in Odoo: Part 4 (CRUD) 3. Read Operation.

--- custom-addons/property/controllers/property_api.py
+++ custom-addons/property/controllers/property_api.py
@@ -56,3 +56,25 @@ class PropertyApi(http.Controller):
             return request.make_json_response({
                 "message": error,
             }, status=400)
+
+    @http.route("/v1/property/<int:property_id>", methods=["GET"], type="http", auth="none", csrf=False)
+    def get_property(self, property_id):
+        try:
+            property_id = request.env['property'].sudo().search([('id', '=', property_id)])
+            if not property_id:
+                return request.make_json_response({
+                    "message": "ID dose not exist!",
+                }, status=400)
+            return request.make_json_response({
+                "id": property_id.id,
+                "ref": property_id.ref,
+                "name": property_id.name,
+                "description": property_id.description,
+                "postcode": property_id.postcode,
+                "date_availability": property_id.date_availability,
+                "expected_selling_date": property_id.expected_selling_date,
+            }, status=200)
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
