dcb7994493c560c1e28a990680769c3295d5c11d
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-08-31 12:40:09 +0300

    Return Merchandise Authorization (RMA) in Odoo.

--- custom-addons/rma_management/__manifest__.py
+++ custom-addons/rma_management/__manifest__.py
@@ -10,7 +10,21 @@
     """,
 
     'data': [
-        "views/base_menu.xml",
+        "security/ir.model.access.csv",
+        "wizard/stock_picking_return_views.xml",
+        "wizard/rma_delivery_views.xml",
+        "wizard/rma_finalization_wizard_views.xml",
+        "wizard/rma_split_views.xml",
+        "views/menus.xml",
+        "views/res_partner_views.xml",
+        "views/rma_finalization_views.xml",
+        "views/rma_portal_templates.xml",
+        "views/rma_team_views.xml",
+        "views/rma_views.xml",
+        "views/rma_tag_views.xml",
+        "views/stock_picking_views.xml",
+        "views/stock_warehouse_views.xml",
+        "views/res_config_settings_views.xml",
     ],
     'application': True,
 }
