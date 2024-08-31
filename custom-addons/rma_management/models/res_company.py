4865fa4be795edeceaa309ad68f1f2c0588d0878
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-08-31 17:24:33 +0300

    Return Merchandise Authorization (RMA) in Odoo.

---
+++ custom-addons/rma_management/models/res_company.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    show_full_page_sale_rma = fields.Boolean(
+        string="Full page RMA creation",
+        help="From the frontend sale order page go to a single RMA page "
+        "creation instead of the usual popup",
+    )
