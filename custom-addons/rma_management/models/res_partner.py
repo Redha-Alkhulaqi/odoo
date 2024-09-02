fe31d047893d81a9c24f459aa5a4608ddfea8709
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-09-02 14:13:27 +0300

    Return Merchandise Authorization (RMA) in Odoo.

--- custom-addons/rma_management/models/res_partner.py
+++ custom-addons/rma_management/models/res_partner.py
@@ -4,7 +4,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    test = fields.Char()
+    test1 = fields.Char()
+    test2 = fields.Char()
     rma_ids = fields.One2many(
         comodel_name="rma",
         inverse_name="partner_id",
