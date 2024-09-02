3f1a525cb5ad5ee1ee3025637a5803a916c42b88
“Redha-Alkhulaqi” <reda.ali.ahmed@gmail.com>
2024-09-02 09:58:46 +0300

    Return Merchandise Authorization (RMA) in Odoo.

---
+++ custom-addons/rma_management/manifest.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Return Merchandise Authorization Management',
+    'summary': 'Return Merchandise Authorization (RMA)',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'website': 'https://electron.sa',
+    'author': 'Electron Technology',
+    'depends': ['stock_account'],
+    "data": [
+        "views/menus.xml",
+    ],
+    "application": True,
+}
--- custom-addons/rma_management/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import rma_delivery
-from . import rma_finalization_wizard
-from . import rma_split
-from . import stock_picking_return
